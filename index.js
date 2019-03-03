11a1dcb06b54f926aa121535a65612e93fa5bcc5
Bjornskjald <github@bjorn.ml>
2019-03-03 22:23:20 +0100

    Initial commit, testing if it works

---
+++ index.js
@@ -0,0 +1,15 @@
+const Docker = require('dockerode')
+const docker = new Docker()
+
+const [ id, token ] = process.argv[2].split('/').slice(5, 7)
+
+const Discord = require('discord.js')
+const hook = new Discord.WebhookClient(id, token)
+let last
+
+setInterval(async () => {
+  const containers = await docker.listContainers()
+  const list = containers.map(({ Names: [ name ], State: state }) => ({ name, state }))
+  console.dir(list)
+  hook.send(JSON.stringify(list, null, 2), { code: true })
+}, 1000)
